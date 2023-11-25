2c42eb6f8eed6e9c4e4dc185b7cf01ee0b18782d
stunnerboyone <stunnerboy691@gmail.com>
2023-11-25 18:35:41 +0200

    first commit

---
+++ TelegramBot/src/main/java/ua/javarush/MyFirstTelegramBot.java
@@ -0,0 +1,36 @@
+package ua.javarush;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.Map;
+
+import static ua.javarush.TelegramBotContent.*;
+import static ua.javarush.TelegramBotUtils.*;
+
+public class MyFirstTelegramBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return "stunnerboyone_jru_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "6832743068:AAFqgjTVrMOW_jaRwbzOMVpAa58ygOw5kf8";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Long chatId = getChatId(update);
+    }
+
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new MyFirstTelegramBot());
+    }
+}
